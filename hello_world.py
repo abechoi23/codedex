e455911b7aa90e8c9014f136c39988002e1afef1
Abe <acec62392@gmail.com>
2023-05-03 12:34:03 -0700

    codedex 1-5

---
+++ hello_world.py
@@ -0,0 +1,27 @@
+""" 02. Hello World
+# Print
+In Python, the print() function is used to tell a computer to "talk." This is something we are going to use a lot.
+
+
+The message we want to display should be inside the parentheses and surrounded by quotes. They can be double quotes " or single quotes ', but the opening and closing quote marks have to be the same.
+
+This is an example of a print() function:
+
+print('👋 Howdy')
+
+In the example above, we instructed our program to print a message. This resulting text that is printed to the screen is referred to as the output.
+
+The output of this example program would be:
+
+👋 Howdy
+
+# Instructions
+Create a new file called hello_world.py using your editor of choice.
+
+On line 1, use print() to output the message "Hello World!"
+
+Save the file and then run it.
+
+Were you able to print "Hello World!" in the terminal?
+ """
+print('Hello World!')
---
+++ initials.py
@@ -0,0 +1,46 @@
+""" 04. Initials
+# Comments
+Comments are very important in programming because they are used to document what our code does. They are also used to disable parts of the program.
+
+When the program is run, the comments are ignored.
+
+In Python, we can create a comment using the # hashtag symbol:
+
+# Printing out a message 
+print('Hi')
+
+On the first line, we created a comment. As a result, everything to the right of the hashtag # is ignored. The program continues to the next line, and the output is simply:
+
+Hi
+
+Python only ignores anything after the # on a line. So when a comment is placed towards the end of a line:
+
+print('Hi')   # I'm a comment, too!
+
+Here, the output would still be "Hi" because everything before the comment on the same line will still run.
+
+# Instructions
+Create an initials.py program that displays your initials in block letters.
+
+First, start your program with a comment that says a fun fact about yourself.
+
+Then, create your block letters with your initials.
+
+For example, if your name is Ada Lovelace, your initials would be A and L, and your block letters would look like this:
+
+  A    L
+ A A   L
+A   A  L
+AAAAA  L
+A   A  L
+A   A  L
+A   A  LLLLL """
+
+
+print('  A      CCCCCCC')
+print(' A  A    C')
+print('A    A   C')
+print('AAAAAA   C')
+print('A    A   C')
+print('A    A   C')
+print('A    A   CCCCCCC')
